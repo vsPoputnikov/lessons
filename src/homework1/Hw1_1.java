d3cadb68ca0fa7a61ea72a37979f05f16cef58d4
Vadim Poputnikov <48300535+vsPoputnikov@users.noreply.github.com>
2019-03-10 23:44:20 +0300

    добавим вторую задачу

--- src/homework1/Hw1_1.java
+++ src/homework1/Hw1_1.java
@@ -1,12 +1,12 @@
 package homework1;
 
-public class hw1_1 {
+public class Hw1_1 {
     public static void main(String[] args) {
-        int length, width, height;
+        int length, width, height; // Три измерения прямоугольного параллелограмма
         length = 3;//длина
         width = 3;//ширина
         height = 4;//высота
-        double s = (length*width + length*height + width*height)*2;
+        double s = (length*width + length*height + width*height)*2; // Площадь поверхности
         System.out.printf("Площадь поверхности параллелепипеда равна %.0f м2 %n", s);
         if ((width > height)) {
             System.out.println("Ширина больше высоты");
---
+++ src/homework1/Hw1_2.java
@@ -0,0 +1,15 @@
+package homework1;
+
+import java.io.PrintStream;
+
+public class Hw1_2 {
+    public static void main(String[] args) {
+        double s, length, width;
+        s = 10; // Площадь в сотках
+        length = 25; // Длина грядки
+        width = 15; // Ширина грядки
+        s *= 100;
+        System.out.printf("Оставшаяся площадь = %.2f м2", (s % (length * width)));
+
+    }
+}
