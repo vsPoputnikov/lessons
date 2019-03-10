c74acef4298a31b071de1e780b4ef05bdf3ad346
Vadim Poputnikov <48300535+vsPoputnikov@users.noreply.github.com>
2019-03-11 00:32:12 +0300

    Задание 5

---
+++ src/homework1/Hw1_5.java
@@ -0,0 +1,21 @@
+package homework1;
+
+public class Hw1_5 {
+    public static void main(String[] args) {
+        double a,b,c,d,x1,x2;
+        a = 1;
+        b = 2;
+        c = -3;
+        d = b*b - 4*a*c;
+        if (d<0){
+            System.out.printf("Уравнение не имеет корней %n");
+        } else if (d>0){
+            x1 = ((-b)+Math.sqrt(d))/(2*a);
+            x2 = ((-b)-Math.sqrt(d))/(2*a);
+            System.out.printf("Уравнение имеет 2 корня: %.2f и %.2f %n",x1,x2);
+        } else {
+            x1=-b/(2*a);
+            System.out.printf("Уравнение имеет 1 корень: %.2f %n", x1);
+        }
+    }
+}
