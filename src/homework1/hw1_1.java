1184940a8f8e28f9600fd2fcb736ef89ac3c645c
Vadim Poputnikov <48300535+vsPoputnikov@users.noreply.github.com>
2019-03-10 23:07:10 +0300

    сравнение измерений

--- src/homework1/hw1_1.java
+++ src/homework1/hw1_1.java
@@ -3,11 +3,19 @@ package homework1;
 public class hw1_1 {
     public static void main(String[] args) {
         int length, width, height;
-        length = 3;
-        width = 3;
-        height = 4;
+        length = 3;//длина
+        width = 3;//ширина
+        height = 4;//высота
         double s = (length*width + length*height + width*height)*2;
         System.out.printf("Площадь поверхности параллелепипеда равна %.0f м2 %n", s);
-
+        if ((width > height)) {
+            System.out.println("Ширина больше высоты");
+        } else {
+            if ((height > width)) {
+                System.out.println("Высота больше ширины");
+            } else {
+                System.out.println("Ширина равна высоте");
+            }
+        }
     }
 }
