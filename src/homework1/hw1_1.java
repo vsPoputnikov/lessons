920aa50f4400165c70c88dead18c5159afc0c353
Vadim Poputnikov <48300535+vsPoputnikov@users.noreply.github.com>
2019-03-10 22:47:28 +0300

    Update hw1_1.java

--- src/homework1/hw1_1.java
+++ src/homework1/hw1_1.java
@@ -3,11 +3,11 @@ package homework1;
 public class hw1_1 {
     public static void main(String[] args) {
         int length, width, height;
-        length = 1;
-        width = 2;
-        height = 1;
+        length = 2;
+        width = 3;
+        height = 4;
         double s = (length*width + length*height + width*height)*2;
         System.out.printf("Площадь поверхности параллелепипеда равна %.0f м2 %n", s);
-        
+
     }
 }
