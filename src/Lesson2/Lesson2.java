db81401207fca576bc4f5ce17ec45a42920c899c
Vadim Poputnikov <vs.poputnikov@gmail.com>
2019-03-11 21:14:19 +0300

    задание 1 дополнено

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -73,7 +73,7 @@ public class Lesson2 {
         // Метки
 
         outer:  for (int i = 0; i < 3; i++) {
-            System.out.println("Внешний цикл");
+            System.out.println("Внешний цикл dsds");
             for (int j = 0; j < 10; j++) {
                 System.out.println("j = " + j);
                 if (j == 5) break outer;
