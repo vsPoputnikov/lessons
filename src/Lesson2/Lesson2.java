aaeb7e5a026c308daa4771c2969b544336ae3cba
Vadim Poputnikov <48300535+vsPoputnikov@users.noreply.github.com>
2019-03-11 19:12:01 +0300

    Добавляем файлы

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -1,6 +1,19 @@
 package Lesson2;
 
+import java.util.Scanner;
+
 public class Lesson2 {
     // Конструкции ветвления
+    public static void main(String[] args) {
+        System.out.println("Вывод информации");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите код");
+        int code = in.nextInt();
 
+        if (code == 123){
+            System.out.println("Вы вошли как админисстратор");
+        } else {
+            System.out.println("Вы не вошли");
+        }
+    }
 }
