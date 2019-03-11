87262920e24bf5e71e128b9183a2136f684a7ca8
Vadim Poputnikov <vs.poputnikov@gmail.com>
2019-03-12 01:27:28 +0300

    problem 1,2

---
+++ src/Homework2/Hw2_1.java
@@ -0,0 +1,25 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class Hw2_1 {
+    public static void res() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите числа для упорядочивания");
+        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt(), buff;
+        for (int i=0;i<2;i++){
+            if (a>b) {
+                buff = b;
+                b = a;
+                a = buff;
+            }
+            if(b>c){
+                buff = c;
+                c = b;
+                b = buff;
+            }
+        }
+        System.out.printf("%d %d %d",a,b,c);
+    }
+
+}
---
+++ src/Homework2/Hw2_2.java
@@ -0,0 +1,9 @@
+package Homework2;
+
+public class Hw2_2 {
+    public static void res(){
+        for(int i=18; i>=0;i--){
+            System.out.printf(" "+i*5);
+        }
+    }
+}
