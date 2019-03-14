5533548ffad54cb61fdeffa4705855e7e8a097a5
Vadim Poputnikov <vs.poputnikov@gmail.com>
2019-03-14 06:24:36 +0300

    homework 3

--- src/homework3/Problem3_7.java
+++ src/homework3/Problem3_7.java
@@ -1,9 +1,12 @@
 package homework3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Problem3_7 {
     public static void solve() {
+        List<Integer> myList = new ArrayList<Integer>();
         Scanner in = new Scanner(System.in).useDelimiter("\\n");
         System.out.println("Введите строку для обработки");
         StringBuilder sbStart = new StringBuilder(in.next());
@@ -11,8 +14,12 @@ public class Problem3_7 {
         //System.out.println(inString);
         String s;
         while (inString.hasNext()) {
+
             s = inString.next();
+            myList.add(Integer.valueOf(s));
             System.out.println(s);
         }
+        System.out.println(myList.toString());
     }
+
 }
