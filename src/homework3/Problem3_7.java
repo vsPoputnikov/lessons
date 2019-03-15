9d58d206620391c015b021de1165b0ccde198c42
Vadim Poputnikov <vs.poputnikov@gmail.com>
2019-03-15 05:57:19 +0300

    переделана задача 3_7

--- src/homework3/Problem3_7.java
+++ src/homework3/Problem3_7.java
@@ -6,19 +6,27 @@ import java.util.Scanner;
 
 public class Problem3_7 {
     public static void solve() {
-//
-        List<Integer> myList = new ArrayList<Integer>();
+
+//        List<Integer> myList = new ArrayList<Integer>();
+//        Scanner in = new Scanner(System.in).useDelimiter("\\n");
+//        System.out.println("Введите строку для обработки");
+//        StringBuilder sbStart = new StringBuilder(in.next());
+//        in = new Scanner(sbStart.toString()).useDelimiter("\\D+");
+//        String s;
+//        while (in.hasNext()) {
+//            s = in.next();
+//            myList.add(Integer.valueOf(s));
+//            System.out.println(s);
+//        }
+//        System.out.println(myList.toString());
         Scanner in = new Scanner(System.in).useDelimiter("\\n");
         System.out.println("Введите строку для обработки");
-        StringBuilder sbStart = new StringBuilder(in.next());
-        in = new Scanner(sbStart.toString()).useDelimiter("\\D+");
-        String s;
-        while (in.hasNext()) {
-            s = in.next();
-            myList.add(Integer.valueOf(s));
-            System.out.println(s);
+        String[] s = in.next().trim().split("\\D+");
+        int[] intArr = new int[s.length];
+        for (int i = 0; i < s.length; i++) {
+            intArr[i] = Integer.valueOf(s[i]);
+            System.out.println(intArr[i]);
         }
-        System.out.println(myList.toString());
     }
 
 }
